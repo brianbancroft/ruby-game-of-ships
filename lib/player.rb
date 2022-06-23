2199379518841bbbe673204999375b8f480a84b2
brianbancroft <hello@brianbancroft.ca>
2022-06-23 16:29:39 -0700

    Create mechanism for subtracting health of ship

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,5 @@
 require_relative 'board'
+require_relative 'ship'
 
 class Player 
     def initialize(name, position)
@@ -6,6 +7,27 @@ class Player
         @name = name
 
         @board = Board.new
+
+            @carrier = Carrier.new,
+            @destroyer = Destroyer.new,
+            @battleship = Battleship.new,
+            @cruiser = Cruiser.new,
+            @submarine = Submarine.new
+        
+    end
+
+    def fleet_status
+        return {
+            carrier: @carrier.alive?,
+            destroyer: @destroyer.alive?,
+            battleship: @battleship.alive?,
+            cruiser: @cruiser.alive?,
+            submarine: @submarine.alive?
+        }
+    end
+
+    def game_over? 
+        !(@carrier.alive? || @destroyer.alive? || @battleship.alive? || @cruiser.alive? || @submarine.alive?)
     end
 
     def board_status
--- lib/ship.rb
+++ lib/ship.rb
@@ -1,59 +1,58 @@
-module Ship
-    @size = 0
-    @name = ""
-    @alive = true
-
-
-    def is_alive
-        return @alive
+class Ship
+    def alive?
+        return @hp.positive?
     end
 
-    def get_name 
-        puts @name
+    def name 
+        return @name
+    end
+
+    def subtract_hp
+        @hp = @hp - 1
     end
 end
 
-class Carrier
+class Carrier < Ship
     def initialize
         @name = "carrier"
         @size = 5
+        @hp = @size
+        @alive = true
     end
-
-    extend Ship
 end
 
-class Battleship
+class Battleship < Ship
     def initialize
         @name = "battleship"
         @size = 4
+        @hp = @size
+        @alive = true
     end
-
-    extend Ship
 end
 
-class Cruiser
+class Cruiser < Ship
     def initialize
         @name = "cruiser"
         @size = 3
+        @hp = @size
+        @alive = true
     end
-
-    extend Ship
 end
 
-class Submarine
+class Submarine < Ship
     def initialize
         @name = 'submarine'
         @size = 3
+        @hp = @size
+        @alive = true
     end
-
-    extend Ship
 end 
 
-class Destroyer 
+class Destroyer  < Ship
     def initialize 
         @name = "destroyer"
         @size = 2
+        @hp = @size
+        @alive = true
     end
-
-    extend Ship
 end
