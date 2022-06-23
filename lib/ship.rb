e5bb606e6902fd8422de15cfe5340ca19d5f9e1c
brianbancroft <hello@brianbancroft.ca>
2022-06-23 16:11:03 -0700

    Add fleet vessels

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,59 @@
-class Ship
-    def initialize()
-        @size = 0
+module Ship
+    @size = 0
+    @name = ""
+    @alive = true
+
+
+    def is_alive
+        return @alive
     end
 
     def get_name 
         puts @name
     end
+end
+
+class Carrier
+    def initialize
+        @name = "carrier"
+        @size = 5
+    end
+
+    extend Ship
+end
+
+class Battleship
+    def initialize
+        @name = "battleship"
+        @size = 4
+    end
+
+    extend Ship
+end
+
+class Cruiser
+    def initialize
+        @name = "cruiser"
+        @size = 3
+    end
+
+    extend Ship
+end
+
+class Submarine
+    def initialize
+        @name = 'submarine'
+        @size = 3
+    end
+
+    extend Ship
+end 
+
+class Destroyer 
+    def initialize 
+        @name = "destroyer"
+        @size = 2
+    end
+
+    extend Ship
 end
